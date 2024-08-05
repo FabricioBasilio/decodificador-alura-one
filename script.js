56fcddecf51d2f3fb047e640f23143d496a03cde
Fabricio Basilio <fabriciobasilioo@gmail.com>
2024-08-05 17:44:36 -0300

    Adicionando as funções criptografar e descriptografar

--- script.js
+++ script.js
@@ -3,43 +3,101 @@ const botaoDescriptografar = document.getElementById("botao-descriptografar");
 const inputDoTexto = document.getElementById("input-texto");
 const imagem = document.getElementById("imagem");
 const mensagem = document.getElementById("mensagem-saida-de-texto");
+const saidaTexto = document.getElementById("saida-texto"); 
 
 botaoCriptografar.addEventListener("click", criptografar);
+botaoDescriptografar.addEventListener("click", descriptografar);
 
 function criptografar() {
     let texto = inputDoTexto.value;
     let estaMinusculo = checarMinusculo(texto);
+    let textoLength = texto.length;
 
-    if (!estaMinusculo) {
-        alert("É necessário o texto estar com letras minúsculas!")
+    if (textoLength === 0) {
+        alert("Não há nenhum texto.");
+        return;
+    }
+    else if (!estaMinusculo) {
+        alert("É necessário o texto estar com letras minúsculas!");
         return;
     }
 
-    let textoLength = texto.length;
+    let textoArray = texto.split("");
 
-    alert(texto)
     for (let i = 0; i < textoLength; i++) {
-        if (texto[i] === "a") {
-            texto[i] = "ai"
+        if (textoArray[i] === "a") {
+            textoArray[i] = "ai"
         }
-        else if (texto[i] === "e") {
-            texto[i] === "enter"
+        else if (textoArray[i] === "e") {
+            textoArray[i] = "enter"
         }
-        else if (texto[i] === "i") {
-            texto[i] === "imes"
+        else if (textoArray[i] === "i") {
+            textoArray[i] = "imes"
         }
-        else if (texto[i] === "o") {
-            texto[i] === "ober"
+        else if (textoArray[i] === "o") {
+            textoArray[i] = "ober"
         }
-        else if (texto[i] === "u") {
-            texto[i] === "ufat"
+        else if (textoArray[i] === "u") {
+            textoArray[i] = "ufat"
         }
     }
 
+    let textoCriptografado = textoArray.join("");
+
     imagem.style.display = "none";
     mensagem.style.display = "none";
+    alert(textoCriptografado);
+    
+}
+
+
+function descriptografar() {
+    let texto = inputDoTexto.value;
+    let estaMinusculo = checarMinusculo(texto);
+    let textoLength = texto.length;
+
+    if (textoLength === 0) {
+        alert("Não há nenhum texto.");
+        return;
+    }
+    else if (!estaMinusculo) {
+        alert("É necessário o texto estar com letras minúsculas!");
+        return;
+    }
+
+    let textoArray = texto.split("");
+
+    for (let i = 0; i < textoLength; i++) {
+        if (textoArray[i] === "ai") {
+            textoArray[i] = "a"
+        }
+        else if (textoArray[i] === "enter") {
+            textoArray[i] = "e"
+        }
+        else if (textoArray[i] === "imes") {
+            textoArray[i] = "i"
+        }
+        else if (textoArray[i] === "ober") {
+            textoArray[i] = "o"
+        }
+        else if (textoArray[i] === "ufat") {
+            textoArray[i] = "u"
+        }
+    }
+
+    let textoDescriptografado = textoArray.join("");
+
+    imagem.style.display = "none";
+    mensagem.style.display = "none";
+    alert(textoDescriptografado);
+    
 }
 
 function checarMinusculo(texto) {
-    return texto === texto.toLowerCase();
+    if (texto === texto.toLowerCase()) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
